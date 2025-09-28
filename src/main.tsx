e2f0a4fb24d7c37534b17d7e1a2e31c450d6add1
RibunLoc <hothanhloc12345@gmail.com>
2025-09-28 15:29:35 +0700

    update(contact-page): fix color text

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,14 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
+import * as Sentry from "@sentry/react";
+
+Sentry.init({
+  dsn: "https://91e7d320ef434fdd7cbf83c1d1f98141@o4509767749664768.ingest.us.sentry.io/4509976206311424",
+  // Setting this option to true will send default PII data to Sentry.
+  // For example, automatic IP address collection on events
+  sendDefaultPii: true
+});
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
